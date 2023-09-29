4f96be61f04f598b119920b3dac4112104d1e1bc
stoozey_ <stoozey123@gmail.com>
2023-09-29 20:02:08 +0100

    added enumz folder support

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+local RunService = game:GetService("RunService");
+
+local Promise = require(script.Parent.promise);
+
 local Enumz = require(script:WaitForChild("enumz"));
 local EnumzTypes = require(script:WaitForChild("enumz_types"));
 
@@ -30,6 +34,45 @@ end
 
 function EnumzManager.Exists(name: string): boolean
 	return (EnumzCache[name] ~= nil);
+end
+
+function EnumzManager.EnumzFolderInitServer(enumzFolder: Folder): nil
+	assert(RunService:IsServer(), "ModulesFolderInitServer should be ran on the server only");
+
+	local children = enumzFolder:GetChildren();
+	for _, child in pairs(children) do
+		if (child:IsA("ModuleScript")) then
+			require(child);
+		end
+	end
+
+	enumzFolder:SetAttribute("TotalChildren", #children);
+end
+
+function EnumzManager.EnumzFolderInitClient(enumzFolder: Folder): Promise<nil>
+	return Promise.new(function(resolve, reject)
+		if (RunService:IsServer()) then return reject("ModulesFolderInitClient should be ran on the client only") end;
+
+		local totalChildren = enumzFolder:GetAttribute("TotalChildren");
+		while (not totalChildren) do
+			task.wait();
+			totalChildren = enumzFolder:GetAttribute("TotalChildren");
+		end
+
+		local children = enumzFolder:GetChildren();
+		while (#children < totalChildren) do
+			task.wait();
+			children = enumzFolder:GetChildren();
+		end
+
+		for _, child in pairs(children) do
+			if (child:IsA("ModuleScript")) then
+				require(child);
+			end
+		end
+
+		resolve();
+	end);
 end
 
 return EnumzManager;
