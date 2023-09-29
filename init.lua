54b2d5b981ac1b6c9edd237b4b0ae5522c8e4324
stoozey_ <stoozey123@gmail.com>
2023-09-29 19:01:12 +0100

    separated Enumz class into separate file

--- init.lua
+++ init.lua
@@ -1,83 +1,4 @@
-export type Enumz = {
-	["Iterator"]: {[number]: string},
-	["Count"]: number,
-	["Random"]: string,
-	["Name"]: string,
-	["ClassName"]: string,
-	["Serialize"]: () -> {},
-	["Exists"]: (value: string) -> boolean
-};
-
-type SerializedEnumz = {
-	["_CN"]: "Enumz",
-	["_EN"]: string
-}
-
-local Enumz = { };
-
-Enumz.__index = function(self, key)
-	if (key == "Iterate") then
-		return function()
-			return self.__valuesIndexed;
-		end
-	elseif (key == "Count") then
-		return #self.__valuesIndexed;
-	elseif (key == "Random") then
-		local count = #self.__valuesIndexed;
-		local index = ((count == 1) and 1 or math.random(1, count));
-		return self.__valuesIndexed[index];
-	elseif (key == "Name") then
-		return self.__name;
-	elseif (key == "ClassType") then
-		return "Enumz";
-	elseif (key == "ClassName") then
-		return self.__name;
-	elseif (key == "Serialized") then
-		return function(): SerializedEnumz
-			return {
-				["_CN"] = "Enumz",
-				["_EN"] = self.__name
-			};
-		end
-	elseif (key == "Exists") then
-		return function(value: string|number)
-			local valueType = typeof(value);
-			assert(((valueType == "string") or (valueType == "number")), ("Invalid value type \"%s\""):format(valueType));
-
-			if (valueType == "string") then
-				return (self.__valuesNamed[value] ~= nil);
-			elseif (valueType == "number") then
-				return (self.__valuesIndexed[value] ~= nil);
-			end
-
-			return false;
-		end
-	end
-
-	local value;
-	if (typeof(key) == "number") then
-		value = self.__valuesIndexed[key];
-	else
-		value = self.__valuesNamed[key];
-	end
-
-	assert(value, ("Enum %s does not have a value matching %s"):format(string(self.__name), string(key)));
-	return value;
-end
-
-function Enumz.new(name: string, values: {string}): Enumz
-	local self = setmetatable({ }, Enumz);
-
-	self.__name = name;
-	self.__valuesIndexed = { };
-	self.__valuesNamed = { };
-	for i, value in pairs(values) do
-		self.__valuesIndexed[i] = value;
-		self.__valuesNamed[value] = i;
-	end
-
-	return self;
-end
+local Enumz = require(script:WaitForChild("Enumz"));
 
 local EnumzManager = { };
 local EnumzCache = { };
@@ -85,7 +6,7 @@ local EnumzCache = { };
 function EnumzManager.Create(name: string, values: {string}): Enumz
 	assert((not EnumzManager.Exists(name)), ("Enum with name %s already exists"):format(name));
 
-	local enumz = Enum.new(name, values);
+	local enumz = Enumz.new(name, values);
 	EnumzCache[name] = enumz;
 
 	return enumz;
